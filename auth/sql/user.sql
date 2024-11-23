55f11dfcc570c61e2a6b05e57bff46826022d7df
Lesion45 <EgorKulagin2005@yandex.ru>
2024-11-23 05:33:09 +0300

    feat: add sql-file

---
+++ auth/sql/user.sql
@@ -0,0 +1,8 @@
+CREATE SCHEMA IF NOT EXISTS users_schema;
+
+CREATE TABLE IF NOT EXISTS users_schema.user (
+     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+     email TEXT UNIQUE,
+     password_hash BYTEA,
+     role TEXT
+);
