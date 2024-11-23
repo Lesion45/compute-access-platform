86390ad2e6170527f23f4c67e71759800b715c9f
Lesion45 <EgorKulagin2005@yandex.ru>
2024-11-23 04:37:13 +0300

    feat: add RegisteredUser and LoggedUser structs

--- auth/internal/entity/user.go
+++ auth/internal/entity/user.go
@@ -7,6 +7,25 @@ import "github.com/google/uuid"
 type User struct {
 	ID           uuid.UUID
 	Email        string
-	HashPassword []byte
+	PasswordHash []byte
 	Role         string
+}
+
+// RegisteredUser represents a user who has been registered in the system
+// but has not yet authenticated. It contains the user's unique ID, email,
+// and role. This structure is typically used during the user registration
+type RegisteredUser struct {
+	ID    uuid.UUID
+	Email string
+	Role  string
+}
+
+// LoggedUser represents an authenticated user. It contains all the fields
+// from RegisteredUser, along with a JWT token that is issued upon successful
+// authentication.
+type LoggedUser struct {
+	ID    uuid.UUID
+	Email string
+	Role  string
+	Token string
 }
